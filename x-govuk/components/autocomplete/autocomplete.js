83109660c4f056619b78ea8904ff900c961cd112
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-08-14 12:41:39 +0100

    Use  for const name in modules

--- x-govuk/components/autocomplete/autocomplete.js
+++ x-govuk/components/autocomplete/autocomplete.js
@@ -1,13 +1,13 @@
 const accessibleAutocomplete = require('accessible-autocomplete')
 
 module.exports = function () {
-  this.start = (element) => {
+  this.start = ($module) => {
     // Autocomplete options get passed from Nunjucks params to data attributes
-    const params = element.dataset
+    const params = $module.dataset
 
     accessibleAutocomplete.enhanceSelectElement({
-      selectElement: element,
-      defaultValue: element.value,
+      selectElement: $module,
+      defaultValue: $module.value,
       autoselect: params.autoselect === 'true',
       displayMenu: params.displayMenu,
       minLength: params.minLength ? parseInt(params.minLength) : 0,
