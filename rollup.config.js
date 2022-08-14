a7f590e2771a11b3458773af04efe7ef4eaa1793
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-08-14 13:14:03 +0100

    Provide bundled JavaScript UMD export

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import commonJs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
+export default [{
+  input: [
+    'x-govuk/all.mjs'
+  ],
+  output: {
+    dir: 'x-govuk',
+    format: 'umd',
+    name: 'GOVUKPrototypeComponents'
+  },
+  plugins: [
+    nodeResolve(),
+    commonJs()
+  ]
+}]
