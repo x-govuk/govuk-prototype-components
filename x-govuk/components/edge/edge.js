bfe8efc91015763467fe6dd28f0fdea9349ea505
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-08-14 12:44:27 +0100

    Call element in parent function of the module

--- x-govuk/components/edge/edge.js
+++ x-govuk/components/edge/edge.js
@@ -1,7 +1,7 @@
 const events = require('eventslibjs')
 
-module.exports = function () {
-  this.init = ($module) => {
+module.exports = function ($module) {
+  this.init = () => {
     const nodes = $module.querySelectorAll('a[href="#"]')
     nodes.forEach(node => { events.on('click', node, alertUser) })
 
