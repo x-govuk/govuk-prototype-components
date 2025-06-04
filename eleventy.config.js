c7af2fd0a098bef674d390dceba2356b6a474223
Frankie Roberto <frankie@frankieroberto.com>
2025-06-04 12:18:01 +0100

    Fix link in service navigation on documentation site

--- eleventy.config.js
+++ eleventy.config.js
@@ -146,6 +146,6 @@ export default function (eleventyConfig) {
       layouts: '_layouts',
       includes: '_components'
     },
-    pathPrefix: process.env.GITHUB_ACTIONS && '/govuk-prototype-components'
+    pathPrefix: process.env.GITHUB_ACTIONS && '/govuk-prototype-components/'
   }
 }
